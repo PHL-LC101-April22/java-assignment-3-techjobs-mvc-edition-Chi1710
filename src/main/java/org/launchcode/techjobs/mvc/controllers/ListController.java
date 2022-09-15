69a21240bf2a5fd19ebd2eff45128f4c0319e61d
Chi Tse <chiyantsestudio@gmail.com>
2022-09-15 17:38:56 -0400

    finish Assignment #3: Tech Jobs (MVC Edition) up to the Bonus Missions

--- src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
@@ -48,7 +48,9 @@ public class ListController {
     }
 
     @GetMapping(value = "jobs")
-    public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam(required = false) String value) {
+    public String listJobsByColumnAndValue(Model model,
+                                           @RequestParam String column,
+                                           @RequestParam(required = false) String value) {
         ArrayList<Job> jobs;
         if (column.equals("all")){
             jobs = JobData.findAll();
